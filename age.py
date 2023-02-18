6ba182d6bd6bc2b2c611d13d5e452d9a743a4ecf
Suhaimee123 <111553531+Suhaimee123@users.noreply.github.com>
2023-02-18 22:07:40 +0700

    Add files via upload

---
+++ age.py
@@ -0,0 +1,34 @@
+###1#####
+# age = 18     
+# name = "John" 
+# print(name)
+# print(age)
+
+
+
+
+######2########
+# age = input
+# name = input
+# print("Your name is",name,"Your age is",age)
+
+
+
+
+######3##########
+# money = float(input("Enter amount of money: "))
+# month = int(input("Enter number of months: "))
+# print(money * month)
+
+
+
+money = float(input("Enter amount of money: "))
+month = int(input("Enter number of months: "))
+year = int(input("Enter number of years: "))
+
+total =  (month + year*12) *money
+print("Total amount of money for", year,"year",month, "monthis", total)
+
+
+
+
